4c586d9641b492887937a49e91d083195cdf8f95
Rizqi Fauzan <fauzangenito@gmail.com>
2022-02-19 10:46:27 +0700

    Add Perkalian Test

--- src/test/java/org/example/PerkalianTest.java
+++ src/test/java/org/example/PerkalianTest.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -13,6 +15,15 @@ public class PerkalianTest {
         WebDriver driver = new ChromeDriver();
         driver.get("https://greyli.github.io/calculator/");
 
+        driver.findElement(By.xpath("//button[.='9']")).click();
+        driver.findElement(By.xpath("//button[.='x']")).click();
+        driver.findElement(By.xpath("//button[.='7']")).click();
+        driver.findElement(By.xpath("//button[@id='resultButton']")).click();
+
+        String output = driver.findElement(By.xpath("//div[@id='output']")).getText();
+
+        Assert.assertEquals("63", output);
+        driver.quit();
     }
 
 
